a7018e9d931d92a5302632c6b41e5d2c171ec0ad
ByCooper <kup.official@yahoo.com>
2023-08-15 22:51:38 +0300

    Курсовая_работа_II(скелет)

---
+++ src/main/java/ru/ByCooper/generatorExamQuestion/GeneratorExamQuestionApplication.java
@@ -0,0 +1,13 @@
+package ru.ByCooper.generatorExamQuestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorExamQuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorExamQuestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ByCooper/generatorExamQuestion/GeneratorExamQuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ByCooper.generatorExamQuestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorExamQuestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
