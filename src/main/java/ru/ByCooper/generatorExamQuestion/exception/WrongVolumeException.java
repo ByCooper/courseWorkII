96b20d5f250489cd254f9752f9ebba51cbac3eda
ByCooper <kup.official@yahoo.com>
2023-08-19 11:28:03 +0300

    Курсовая_работа_II(базовая сложность)

--- src/main/java/ru/ByCooper/generatorExamQuestion/exception/WrongVolumeException.java
+++ src/main/java/ru/ByCooper/generatorExamQuestion/exception/WrongVolumeException.java
@@ -1,5 +1,9 @@
 package ru.ByCooper.generatorExamQuestion.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class WrongVolumeException extends RuntimeException{
     public WrongVolumeException() {
     }
